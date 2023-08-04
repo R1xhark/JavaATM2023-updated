26d22143c31540a86a5de6bccd9c51d9be1bb612
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-08-04 14:47:32 +0200

    Update LibertyDatabaseConnector.java
    Creating algorytm for adding clients

--- LibertyDatabaseConnector.java
+++ LibertyDatabaseConnector.java
@@ -8,11 +8,38 @@ import java.sql.SQLException;
     private String jdbcHost="hostname";
     private String dbPassword="password";
     private String dbUsername="user";
+    bool clientCreated=false;
 
-   try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(jdbcHost, dbPassword, dbUsername);
-            connection.close();
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+    public void AddClient(int pin, int id, string name,int cardnumber){
     
+       try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connector = DriverManager.getConnection(jdbcHost, dbPassword, dbUsername);
+
+            newClient Klient=new newClient(pin,id,name,cardnumber);
+        
+            String insertQuery = "INSERT INTO ClientInformation (client_name, card_number, pin,client_id) VALUES (?, ?, ?, ?)";
+            PreparedStatement inserter = connection.prepareStatement(insertQuery);
+
+            preparedStatement.setString(1, newClient.getName());
+            preparedStatement.setString(2, newClient.getCardNumber());
+            preparedStatement.setString(3, newClient.getPin());
+            preparedStatement.setString(4, newClient.getId());
+     
+        
+            int affected = inserter.executeUpdate();
+            if (rowsAffected > 0) {
+            System.out.println("Client byl úspěšně vytvořen!");
+            clientCreated=true;
+             
+            }  
+
+            inserQuery.close();
+            connector.close();
+       }
+            catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Uzivatel nebyl vytvoren");
+            e.printStackTrace();
+       }
+    }
+ 
