289aab6370d3335ae1d5b4e56d7ebefc4f9bec05
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-08-04 14:17:54 +0200

    Update and rename atm.java to createAcc.java

---
+++ createAcc.java
@@ -0,0 +1,8 @@
+import java.util.Set;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Random;
+
+public class CreateAccount {
+      System.Out.Println("Dobrý den. Vítejte v Liberty banks.
+  
