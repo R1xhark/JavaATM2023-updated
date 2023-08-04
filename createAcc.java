6cf8bb1a3a36a96ca9d0d13bca51916ec8051ad5
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-08-04 14:27:08 +0200

    Update createAcc.java
    Making new class for creating an account

--- createAcc.java
+++ createAcc.java
@@ -2,7 +2,26 @@ import java.util.Set;
 import java.io.*;
 import java.util.Scanner;
 import java.util.Random;
+import Liberty.Sqlconnector;
 
-public class CreateAccount {
-      System.Out.Println("Dobrý den. Vítejte v Liberty banks.
-  
+public class NewClient {
+
+      private string name;
+      private int id;
+      private int card_number;
+      private int pin;
+      private int age;
+      
+      Scanner read=new Scanner();
+      System.Out.Println("Dobrý den. Vítejte v Liberty banks.")
+      
+
+      public void createAcc(){
+      System.Out.Println("Zadejte vaše jméno");
+      name=read.NextLine();
+      System.Out.Println("Zadejte prosím svůj věk");
+      age=read.NextLine();
+
+      if(age > 18 && age < 99 && age > 0){
+            Statement nameAdd=
+            
