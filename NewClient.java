79a74375b3291fced07b51ecd98e61a33c4d2909
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-08-05 17:58:03 +0200

    Update NewClient.java
    Updated Security

--- NewClient.java
+++ NewClient.java
@@ -1,9 +1,5 @@
 package com.mycompany.banksim;
 
-/**
- *
- * @author R1xhark
- */
 import java.util.Scanner;
 import java.util.Random;
 
@@ -12,44 +8,59 @@ public class NewClient {
     private String name;
     private int id;
     private long cardNumber;
-    private int pin;
     private int age;
 
     public void createAcc() {
-        try (Scanner read = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Dobrý den. Vítejte v Liberty banks.");
-            
+
             //input: Jmeno Klienta
             System.out.println("Zadejte vaše jméno");
-            name = read.nextLine();
+            name = scanner.nextLine();
+
             // input pro vek klienta, klient mladsi 18 let preci nemuze mit ucet
             System.out.println("Zadejte prosím svůj věk");
-            age = read.nextInt();
-            
-            // je li starsi 18 let muze si vytvorit ucet!
+            age = scanner.nextInt();
+
+            // je li starsi 18 let muze si vytvorit ucet
             if (age > 18 && age < 99 && age > 0) {
-                Random randomizer=new Random();
-                
+                Random randomizer = new Random();
+
                 //Vygenereovani nahodneho 6-mistneho id
-                id=randomizer.nextInt(1000000)+1000000;
-                //Vygenerovani nahodneho 16-mistneho id
-                cardNumber=(long) (randomizer.nextDouble() * 9000000000000000L) + 1000000000000000L;;
-                
-                System.out.println("Prosim vytvorte si 4 mistny pin:");
-                pin=read.nextInt();
-                
-                int pinCheck=String.valueOf(pin).length();
-                
-                if(pinCheck==4){
-                LibertyDatabaseConnector clientCreator=new LibertyDatabaseConnector();
+                id = randomizer.nextInt(1000000) + 1000000;
+
+                //Vygenerovani nahodneho 16-mistneho cisla bankovni karty
+                cardNumber = (long) (randomizer.nextDouble() * 9000000000000000L) + 1000000000000000L;
+
+                // Bezpecnejsi input mechanismus pro PIN
+                int pin = secureInputForPin(scanner);
+
+                // pridani klienta do databaze
+                LibertyDatabaseConnector clientCreator = new LibertyDatabaseConnector();
                 clientCreator.addClient(name, cardNumber, pin, id);
-                }
-                else{
-                    System.out.println("Neplatny pin!!");
-                }
             } else {
-                System.out.println("Věk nesplňuje požadavky pro vytvoření účtu.");
+                System.out.println("Věk nesplňuje požadavky pro vytvoření účtu."); // nesplnuje vek > 18
             }
         }
+    }
+
+    // bezpecne zadani pinu
+    private int secureInputForPin(Scanner scanner) {
+        int pin = 0;
+        boolean validInput = false;
+        
+        while (!validInput) {
+            System.out.println("Prosím vytvořte si 4-místný PIN:");
+            String pinString = scanner.next();
+            
+            if (pinString.matches("\\d{4}")) {
+                pin = Integer.parseInt(pinString);
+                validInput = true;
+            } else {
+                System.out.println("Neplatný PIN. Zadejte znovu.");
+            }
+        }
+        
+        return pin;
     }
 }
