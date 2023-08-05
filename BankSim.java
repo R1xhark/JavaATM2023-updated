2f398588a227be65eadfb9216dcc5e916decbe82
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-08-05 18:21:03 +0200

    Update BankSim.java
    Odstranen nevyuziti import java.sql.exception a pridano volani objektu LibertyDatabaseConnector.firstRun ktery pokud v nasi databazi neni tabulka se kterou program pracuje tak ji vytvori pomoci SQL skriptu.

--- BankSim.java
+++ BankSim.java
@@ -1,6 +1,5 @@
 package com.mycompany.banksim;
 
-import java.sql.SQLException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,26 +7,32 @@ public class BankSim {
 
     public static void main(String[] args) {
         ATM atm = new ATM();
+        LibertyDatabaseConnector firstRun=new LibertyDatabaseConnector();
         NewClient klient = new NewClient();
         Scanner read = new Scanner(System.in);
 
+        // firstRun zkontroluje jestli je v databazi tabulka se kterou program pracuje..
+        System.out.println("------------------");
+        System.out.println("Nacitam databazy");
+        firstRun.createTableInDatabase();
+        System.out.println("");
+        
         System.out.println("Co si prejes?");
         System.out.println("1. Vytvor clienta");
         System.out.println("2. Emuluj ATM");
         System.out.println("------------");
         System.out.println("Tvuj vyber?");
-
+        
         try {
             int mainMenuInput = read.nextInt();
-            read.nextLine(); // Consume the newline
+            read.nextLine(); 
             switch (mainMenuInput) {
                 case 1 -> klient.createAcc();
                 case 2 -> atm.start();
                 default -> System.out.println("Neplatna volba. Zvolte 1 pro vytvoreni clienta nebo 2 pro emulaci ATM.");
             }
    } catch (InputMismatchException e) {
-    System.out.println("Neplatny vstup. Zadejte pouze cislo.");
+    System.out.println("Neplatny vstup. Zadejte platne cislo.");
 }
     }
 }
-
