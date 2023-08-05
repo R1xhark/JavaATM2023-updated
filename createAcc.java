e05606299e340604a830e95346a332f0cbc08a3c
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-08-05 11:09:37 +0200

    Update createAcc.java
    add card and id generation
    added if else for age verification
    added feeding input data into database connector

--- createAcc.java
+++ createAcc.java
@@ -1,27 +1,45 @@
-import java.util.Set;
-import java.io.*;
+package com.mycompany.banksim;
+
+/**
+ *
+ * @author R1xhark
+ */
 import java.util.Scanner;
 import java.util.Random;
-import Liberty.Sqlconnector;
 
 public class NewClient {
 
-      private string name;
-      private int id;
-      private int card_number;
-      private int pin;
-      private int age;
-      
-      Scanner read=new Scanner();
-      System.Out.Println("Dobrý den. Vítejte v Liberty banks.")
-      
+    private String name;
+    private int id;
+    private long cardNumber;
+    private int pin;
+    private int age;
 
-      public void createAcc(){
-      System.Out.Println("Zadejte vaše jméno");
-      name=read.NextLine();
-      System.Out.Println("Zadejte prosím svůj věk");
-      age=read.NextLine();
-
-      if(age > 18 && age < 99 && age > 0){
-            Statement nameAdd=
+    public void createAcc() {
+        try (Scanner read = new Scanner(System.in)) {
+            System.out.println("Dobrý den. Vítejte v Liberty banks.");
             
+            //input: Jmeno Klienta
+            System.out.println("Zadejte vaše jméno");
+            name = read.nextLine();
+            // input pro vek klienta, klient mladsi 18 let preci nemuze mit ucet
+            System.out.println("Zadejte prosím svůj věk");
+            age = read.nextInt();
+            
+            // je li starsi 18 let muze si vytvorit ucet!
+            if (age > 18 && age < 99 && age > 0) {
+                Random randomizer=new Random();
+                
+                //Vygenereovani nahodneho 6-mistneho id
+                id=randomizer.nextInt(1000000)+1000000;
+                //Vygenerovani nahodneho 16-mistneho id
+                cardNumber=(long) (randomizer.nextDouble() * 9000000000000000L) + 1000000000000000L;;
+                
+                LibertyDatabaseConnector clientCreator=new LibertyDatabaseConnector();
+                clientCreator.AddClient(name, cardNumber, pin, id);
+            } else {
+                System.out.println("Věk nesplňuje požadavky pro vytvoření účtu.");
+            }
+        }
+    }
+}
