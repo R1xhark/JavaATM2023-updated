e89af9894f361f35bfd9736aed40c828e65b5292
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-08-05 18:22:24 +0200

    Update and rename createDatabaseScript.SQL to LibertyBankDatabase.sql

--- LibertyBankDatabase.sql
+++ LibertyBankDatabase.sql
@@ -2,7 +2,8 @@ CREATE TABLE ClientInformation (
     client_id INT AUTO_INCREMENT PRIMARY KEY,
     client_name VARCHAR(255) NOT NULL,
     card_number VARCHAR(16) NOT NULL,
-    pin VARCHAR(4) NOT NULL
+    pin VARCHAR(4) NOT NULL,
+    balance INT
 );
 CREATE VIEW ClientView AS
 SELECT client_id, client_name, card_number
