28dd775154e15c526af847bb857e6f6dbc50a39e
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-08-05 11:31:00 +0200

    Update LibertyDatabaseConnector.java
    small update in formating

--- LibertyDatabaseConnector.java
+++ LibertyDatabaseConnector.java
@@ -1,44 +1,44 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
- public class LibertyDatabaseConnector(){
-
+ public class LibertyDatabaseConnector{
+    
+    
     private String jdbcHost="hostname";
     private String dbPassword="password";
     private String dbUsername="user";
-    bool clientCreated=false;
 
-    public void AddClient(int pin, int id, string name,int cardnumber){
+    public void AddClient(String client_name, long card_number, int pin,int client_id){
     
        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connector = DriverManager.getConnection(jdbcHost, dbPassword, dbUsername);
-
-            newClient Klient=new newClient(pin,id,name,cardnumber);
-        
-            String insertQuery = "INSERT INTO ClientInformation (client_name, card_number, pin,client_id) VALUES (?, ?, ?, ?)";
-            PreparedStatement inserter = connection.prepareStatement(insertQuery);
-
-            preparedStatement.setString(1, name);
-            preparedStatement.setInt(2, cardNumber);
-            preparedStatement.setInt(3, pin);
-            preparedStatement.setInt(4, id);
-     
-        
-            int affected = inserter.executeUpdate();
-            if (rowsAffected > 0) {
-            System.out.println("Client byl úspěšně vytvořen!");
-            clientCreated=true;
-             
-            }  
-
-            inserQuery.close();
-            connector.close();
+               try (Connection connector = DriverManager.getConnection(jdbcHost, dbPassword, dbUsername)) {
+               
+               
+               String insertQuery = "INSERT INTO ClientInformation (client_name, card_number, pin,client_id) VALUES (?, ?, ?, ?)";
+               PreparedStatement inserter = connector.prepareStatement(insertQuery);
+               
+               PreparedStatement.setString(1, client_name);
+               PreparedStatement.setLong(2, card_number);
+               PreparedStatement.setInt(3, pin);
+               PreparedStatement.setInt(4, Client_id);
+               
+               
+               int affected = inserter.executeUpdate();
+               if (rowsAffected > 0) {
+                   System.out.println("Client byl úspěšně vytvořen!");
+                   
+               }
+               
+               insertQuery.close();
+           }
        }
             catch (ClassNotFoundException | SQLException e) {
             System.out.println("Uzivatel nebyl vytvoren");
             e.printStackTrace();
        }
     }
- 
+ }
+
